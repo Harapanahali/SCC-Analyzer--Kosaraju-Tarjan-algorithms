9d40ea42a40513df19b6c6ba1b2aa9944e4aff14
Harapanahalli ashwini <ashwiniharapanahalli605@gmail.com>
2026-08-10 12:21:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base:'/
+    SCC-Analyzer--Kosaraju-Tarjan_algorithms/',
   plugins: [react()],
 })
